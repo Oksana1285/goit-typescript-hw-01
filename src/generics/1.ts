84ada6845ee19d86286eea8fd18be7008ee9c903
Oksana Kolesova <kolesovaok85@gmail.com>
2025-03-10 19:59:35 +0200

    hw ts1.3

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -4,7 +4,7 @@ type Tudu = { userId: 1; id: 1; title: 'delectus aut autem'; completed: false };
 
 async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
